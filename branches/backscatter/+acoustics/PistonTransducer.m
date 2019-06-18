35c659b35b71251df758e1d94c36125899555bf7
bartvermeulen <bartvermeulen@db09c8cb-a954-4599-9796-730ab44c0366>
2019-06-18 14:38:00 +0000

    Added classes for common acoustic computations

---
+++ branches/backscatter/+acoustics/PistonTransducer.m
@@ -0,0 +1,190 @@
+classdef PistonTransducer < handle
+    % acoustics.PistonTransducer to get properties of piston transducer.
+    %   This class aids in computing characteristics of piston transducers.
+    %   Most computations are based on the book by Medwin and Clay.
+    % 
+    % acoustics.PistonTransducer properties:
+    %   radius - radius of transducer in m (default is 0.0505)
+    %   frequency - frequency of the transducer in Hz (default is 614.4e3)
+    %   depth - depth of transducer in m (default is 1)
+    %   water - acoustics.Water object describing properties of water
+    %
+    % acoustics.PistonTransducer dependent properties (read only):
+    %   wavelength - wavelength in m
+    %   wavenumber - angular wave number of the sound waves in 1/m
+    %   nearfield - range of near field in m
+    %   angularfreq - angular frequency of the sound waves in rad/s
+    %   attenuation - sound attenuation caused by water in dB/m
+    %   attenuation_e - sound attenuation caused by water in 1/m (neper)
+    %   speedsound - speed ouf sound in water in m/s
+    %
+    % acoustics.PistonTransducer methods:
+    %   directivity - Computes the directivity of the emitted sound
+    %   dir_response - Compute the direction response of the transducer
+    %   near_field_correction - Compute the near field correction factor
+    %   side_lobes - Compute angle of transmitted side lobes
+    %   zeros - Compute angle where no sound it transmitted
+    
+    properties
+        % radius - Radius of the transducer in m
+        %   This should be a positive, double, scalar value. Default:
+        %   0.0505
+        radius(1,1) double {mustBePositive} = 0.0505 
+        
+        % frequency - Frequency of the emitted sound in Hz
+        %   This should be a positive, double, scalar value. Default:
+        %   614.4e3
+        frequency(1,1) double {mustBePositive} = 614.4e3 
+        
+        % depth - Depth of the transducer in m
+        %   This should be a positive, double, scalar value. Default: 1
+        depth(1,1) double {mustBePositive} =1
+        
+        % water - Properties of water
+        %   This is a scalar object of class acoustics.Water. Holds common
+        %   properties of water
+        %
+        %   see also: acoustics.Water
+        water(1,1) acoustics.Water = acoustics.Water() 
+    end
+    properties(Dependent, GetAccess=public, SetAccess=private)
+        wavelength %The wave length of the sound produced by the transducer in m.
+        wavenumber %The (angular) wave number of the sound produced by the transducer in 1/m.
+        nearfield %The near field range in m. After this range the planar wave assumption holds.
+        angularfreq % The angular frequency of the sound produced by the transducer in rad/s.
+        attenuation % Sound attenuation in water in dB/m
+        attenuation_e % Sound attenuation in 1/m (neper)
+        speedsound % Speed of sound in the given water m/s
+    end
+    properties(Dependent, Access=private)
+       f1,f2,A1,A2,A3,P2,P3
+    end
+    properties(Constant, Access=private)
+        P1=1;                                                               % -, Medwin and clay, pg 110, eq 3.4.30
+        besjzeros=[3.83171 7.01559 10.17347 13.32369 16.47063 19.61586...
+            22.76008 25.90367 29.04683 32.18968 35.33231 38.47477...
+            41.61709 44.75932 47.90146 51.04354 54.18555 57.32753...
+            60.46946 63.61136];                                             % Zeros of bessel function j1 (from Abramowitz and Stegun table 9.5)
+    end
+    methods(Access=public)
+        function Di=directivity(obj,phi)
+            % Directivity of the transducer 
+            % 
+            % DI=obj.directivity(PHI) computes the directivity (-) as a function 
+            %   of the angle phi  (radians) from the transducer axis. Output
+            %   DI will have same size as input PHI.
+            assert(isnumeric(phi),'Phi should be numerical')
+            Di=2*besselj(1,obj.wavenumber*obj.radius.*sin(phi))./(obj.wavenumber*obj.radius.*sin(phi));
+        end
+        function Dr=dir_response(obj,phi)
+            % Directional response of the transducer
+            %
+            % DR=obj.dir_response(PHI) returns the directional responce in 
+            %   dB of the transducer as a function of the angle PHI (rad) 
+            %   from the transducer axis. Output DR has the same size as
+            %   input PHI.
+            assert(isnumeric(phi),'Phi should be numerical')
+            Dr=10*log10(obj.directivity(phi).^2);
+        end       
+        function psi=near_field_correction(obj,range)
+            % Returns the near field correction factor for the transducer
+            %
+            % PSI=obj.near_field_correction(RANGE) returns the near field
+            %  correction factor PSI (-) (Downing et al. 1995), to correct for 
+            %  interference in the nearfield of the transducer as a function of
+            %  the RANGE from the transducer. Output PSI will have the same
+            %  size as input RANGE
+            assert(isnumeric(range),'Range should be numerical')
+            z=range/obj.nearfield;
+            psi=(1+1.32*z+(2.5*z).^3.2)./(1.32*z+(2.5*z).^3.2);
+        end
+        function ang=zeros(obj,n)
+            % Computes the angles at which no sound is transmitted
+            %
+            % ang=obj.zeros(n) with n being the n-th zero, returns the
+            % angle of the n-th zero from the axis of the acosutic beam
+            % in radians
+            assert(isnumeric(n) && all(uint32(n(:))==n(:)) && all(n(:)>0) && all(n(:)<21),'n must be an integer between 1 and 20')
+            ang=asin(obj.besjzeros(n)/obj.wavenumber/obj.radius);
+        end
+        function ang=side_lobes(obj,n)
+            % Computes the angles at which side lobes are transmitted
+            %
+            % ang=obj.side_lobes(n) with n being the n-th size lobe, returns the
+            % angle of the n-th zero from the axis of the acosutic beam
+            % in radians. For n=0 return the angle of the main lobe, i.e. 0
+            assert(isnumeric(n) && all(uint32(n(:))==n(:)) && all(n(:)>=0) && all(n(:)<20),'n must be an integer between 0 and 19')
+            ang=nan(size(n));
+            ang(n==0)=0;
+            for ci=unique(n(n>0))
+                ang(n==ci)=fminbnd(@(x) -(obj.directivity(x).^2),obj.zeros(ci),obj.zeros(ci+1),optimset('TolX',1e-6));
+            end
+        end
+    end
+    methods        
+        %% Property get methods for public, dependent properties
+        function value=get.speedsound(obj)
+            value=obj.water.speedsound(obj.depth);
+        end
+        function value=get.wavelength(obj)
+            value=obj.speedsound/obj.frequency;
+        end
+        function value=get.wavenumber(obj)
+            value=2*pi*obj.frequency./obj.speedsound;
+        end
+        function value=get.nearfield(obj)
+            value=pi*obj.radius^2./obj.speedsound*obj.frequency;
+        end
+        function value=get.angularfreq(obj)
+            value=2*pi*obj.frequency;
+        end
+        function value=get.attenuation(obj)
+           freq=obj.frequency/1000;
+           value=(obj.A1*obj.P1*obj.f1*freq^2./(freq^2+obj.f1.^2)+...
+                  obj.A2*obj.P2*obj.f2*freq^2./(freq^2+obj.f2.^2)+...
+                  obj.A3*obj.P3*freq^2)/1000;                     % dB/m, Medwin and clay, pg 109, eq 3.4.29
+        end
+        function value=get.attenuation_e(obj)
+            value=obj.attenuation/8.68;                                    % Medwin and clay, pg104, eq 3.4.8b
+        end
+        %% Property get methods for protected or private properties
+        % Boric acid components in Sea Water
+        function value=get.A1(obj)
+            value=8.68./obj.speedsound.*10.^(0.78*obj.water.pH-5);               % dB/km/kHz, Medwin and clay, pg 110, eq 3.4.30
+        end
+        function value=get.f1(obj)
+            S=obj.water.salinity/1000;
+            value=2.8*(S/35).^.5.*10.^(4-1245/(273+...
+                obj.water.temperature));                                         % kHz, Medwin and clay, pg 110, eq 3.4.30
+        end
+        % Magnesium Sulfate components in Sea Water
+        function value=get.A2(obj)
+            S=obj.water.salinity/1000;
+            value=21.44*S./obj.speedsound.*(1+...
+                0.025*obj.water.temperature);                                    % dB/km/kHz, Medwin and clay, pg 110, eq 3.4.31
+        end
+        function value=get.P2(obj)
+            value=1-1.37e-4.*obj.depth+6.2e-9.*obj.depth.^2;               % - , Medwin and clay, pg 110, eq 3.4.31
+        end   
+        function value=get.f2(obj)
+            S=obj.water.salinity/1000;
+            value=8.17*10.^(8-1990./(273+obj.water.temperature))./(1+...
+                0.0018*(S-35));                                 % kHz., Medwin and clay, pg 110, eq 3.4.31
+        end
+        %Pure Water constants
+        function value=get.A3(obj)
+            value=obj.speedsound*nan;
+            fcold=obj.water.temperature<=20;
+            value(fcold)=4.937e-4-2.59e-5*obj.water.temperature(fcold)+...
+                9.11e-7*obj.water.temperature(fcold).^2-...
+                1.5e-8*obj.water.temperature(fcold).^3;                          % dB/km/kHz, Medwin and clay, pg 110, eq 3.4.32
+            fcold=~fcold;
+            value(fcold)=3.964e-4-1.146e-5*obj.water.temperature(fcold)+...
+                1.45e-7*obj.water.temperature(fcold).^2-...
+                6.5e-10*obj.water.temperature(fcold).^3;                         % dB/km/kHz, Medwin and clay, pg 110, eq 3.4.33
+        end
+        function value=get.P3(obj)
+            value=1-3.83e-5*obj.depth+4.9e-10*obj.depth.^2;                % -, Medwin and clay, pg 110, eq 3.4.34
+        end
+    end
+end
---
+++ branches/backscatter/+acoustics/Sediment.m
@@ -0,0 +1,179 @@
+classdef Sediment < handle
+% Sediment class
+%   Query objects of this class to obtain percentiles of the grain size 
+%   distribution, the parameters of the normal phi (-log2(diameter in mm))
+%   distribution and the parameters of the lognormal distribution of the
+%   sediment diameters in m.
+%
+%   OBJ=Sediment Constructs a default object with the properties set to
+%           their default values
+%   OBJ=Sediment(DISTRIB) Creates an object with the given distribution.
+%           For the format of this variable see Properties
+%   OBJ=Sediment(DISTRIB,DENSITY) Creates and object with given
+%           distribution and density
+%
+%   Sediment Properties:
+%       density     - Density of specific weight of the sediment in kg/m^3
+%                     or g/L. Defualt is 2650
+%       distrib     - m by 2 matrix defining the known percentiles of the 
+%                     sediment distribution. The first column contains the
+%                     percantage of sediment with a size smaller than the
+%                     size given in the second collumn in m. The matrix
+%                     must contain at least two rows. The default is: [50
+%                     300e-6; 90 450e-6]. This means: D50 = 300 micron,
+%                     D90=450 micron. 
+%       normParms   - Paramters (mu and sigma) of the normal distribution 
+%                     in phi units (Get only property)
+%       lognParms   - Parameters (mu and sigma) of the lognormal 
+%                     distribution in m (get only property)
+%
+%   Sediment Methods:
+%       plot        - Plots the fitted sediment distribution and the given
+%                     quantiles in DISTRIB
+%       D(n)        - Returns the size in m for a certain quantile. n in
+%                     percentage
+%       lognM(N)    - Returns the Nth moment of the lognormal distribution
+%
+
+%   Author: Bart Vermeulen
+%   Last edit: 13-04-2010
+
+    properties
+        density=2650 %Sediment density in kg/m^3
+    end
+    properties(SetObservable, AbortSet)
+        distrib=[50 300e-6;...  % Grain size distribution (quantiles in %, second column size in m) 
+                 90 450e-6]
+    end
+    properties(Dependent)
+        normParms  % Parameters for the normal phi probability distribution
+        E
+        VAR
+    end
+    properties(GetAccess=public, SetAccess=private)
+        lognParms   % Parameters for the lognormal grain size distribution in m
+    end
+    methods
+        %% Constructor
+        function obj=Sediment(varargin)
+            if nargin == 0
+                setgrdist(obj);
+            end
+            addlistener(obj,'distrib','PostSet',@obj.setgrdist);
+            if nargin > 0, obj.distrib=varargin{1};end
+            if nargin == 2,obj.density=varargin{2};end
+            assert(nargin<3,'Too many input arguments');
+        end
+        %% Propety access methods
+        function value=get.normParms(obj)
+            value(1)=-((obj.lognParms(1)+3*log(10))/log(2)+1);
+            value(2)=obj.lognParms(2)/log(2);
+        end
+        function value=get.E(obj)
+            value=obj.lognM(1);
+        end
+        function value=get.VAR(obj)
+            value=obj.lognM(2);
+        end
+        function set.density(obj,value)
+            if isnumeric(value) && isscalar(value) && value>0
+                obj.density=value;
+            else
+                error('sediment:setDensity:BadInput','Density should be numerical positive scalar')
+            end
+        end
+        function set.distrib(obj,value)
+            if isnumeric(value) && size(value,2)==2 && size(value,1)>1 && issorted(value(:,1)) && issorted(value(:,2)) && all(value(:)>0) && all(value(:,1)<100)
+                obj.distrib=value;
+            else
+                error('sediment:setDistrib:BadInput','Wrong format for distrib')
+            end
+        end
+        %% Generic methods
+        function value=lognM(obj,n)
+            % OBJ.lognM(N) Determines the Nth central moment of the lognormal
+            %   distribution of grain size distribution in m
+            
+            % Last edit: 14-04-2010
+            mu=obj.lognParms(1);
+            sigma=obj.lognParms(2);
+            switch n
+                case 1
+                    value=exp(mu+sigma^2/2);
+                case 2
+                    value=(exp(sigma^2)-1)*exp(2*mu+sigma^2);
+                case 3
+                    value=(exp(sigma^2)+2)*sqrt(exp(sigma^2)-1);
+                case 4
+                    value=exp(4*sigma^2)+2*exp(3*sigma^2)+3*exp(2*sigma^2)-3;
+                otherwise
+                    value=nan;
+            end
+        end
+        function value=lognrawM(obj,n)
+            % OBJ.lognrawM(N) Determines the Nth raw moment of the lognormal
+            %   distribution of grain size distribution in m
+            
+            % Last edit: 14-04-2010
+            mu=obj.lognParms(1);
+            sigma=obj.lognParms(2);
+            value=exp(n*mu+n^2*sigma^2/2);
+        end
+
+        function value=D(obj,n) 
+            % OBJ.D determines quantiles of the sediment grain size
+            % distribution
+            %
+            % SIZE=OBJ.D(N) Returns the sediment size such that N % of the 
+            %       sediment is smaller. E.g. OBJ.D(50) returns the median
+            %       grain size for the estimated distribution
+            %
+          
+            % Last edit 13-04-2010
+            value=logninv(n/100,obj.lognParms(1),obj.lognParms(2));
+        end
+        function plot(obj)
+           % OBJ.PLOT plots the estimated cdf and the given distribution
+           %
+           % OBJ.plot Creates the plot
+           
+           % Last edit: 13-04-2010
+           x=linspace(obj.D(0.01),obj.D(99.9),1000);
+           y=logncdf(x,obj.lognParms(1),obj.lognParms(2));
+           plot(-log2(x*1000),y)
+           hold on
+           plot(-log2(obj.distrib(:,2)*1000), obj.distrib(:,1)/100,'rx');
+           set(gca,'xdir','reverse')
+           xlabel('Grain size in phi units \leftarrow')
+           ylabel('Cumulative probability \rightarrow')
+           hold off
+        end
+    end
+    %% Private methods
+    methods(Access=private)
+        function perf=lgnfit(obj,x)
+            % Determines sum of squared differences between known quantiles
+            % and the fit
+            if x(2)<0 
+                perf=Inf;
+            else
+                perf=nansum((obj.distrib(:,1)/100-logncdf(obj.distrib(:,2),x(1),x(2))).^2);
+            end
+        end
+        function setgrdist(obj,~,~)
+            % Estimates the parameters of the lognormal grain size
+            % distribution
+            D84=interp1(obj.distrib(:,1),obj.distrib(:,2),84.1,'cubic','extrap')*1000;
+            D16=interp1(obj.distrib(:,1),obj.distrib(:,2),15.9,'cubic','extrap')*1000;
+            muphi=-log2(sqrt(D84*D16));
+            sigphi=log2(sqrt(D84/D16));
+            x0(1)=-muphi*log(2)-3*log(10);
+            x0(2)=sigphi*log(2);
+            [obj.lognParms,~,xflag,outpt]=fminsearch(@obj.lgnfit,x0,optimset('Display','off','TolX',1e-6));
+            if xflag ~=1
+                warning('sediment:setgrdist:BadEstimate',['Could not find a good distribution fit: ',outpt.message])
+            end
+        end
+    end
+    
+end
