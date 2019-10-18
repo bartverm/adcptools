e2c6bf8aa25f3fe155654405e22cc68195e6b9a6
bartvermeulen <bartvermeulen@db09c8cb-a954-4599-9796-730ab44c0366>
2019-10-18 13:05:47 +0000

    Added documentation for GrainSizeDistribution and added attenuation computations

--- branches/backscatter/+acoustics/GrainSizeDistribution.m
+++ branches/backscatter/+acoustics/GrainSizeDistribution.m
@@ -1,15 +1,16 @@
 classdef GrainSizeDistribution < handle
     properties
-        grainsize(:,1) double {mustBeNonnegative, mustBeNonNan, mustBeNonempty} = [0; inf]
+        diameter(:,1) double {mustBeNonnegative, mustBeNonNan, mustBeNonempty} = [0; inf]
         fraction(:,1) double {mustBeFinite, mustBeNonempty, mustBeGreaterThanOrEqual(fraction,0), mustBeLessThanOrEqual(fraction, 1)}=1
     end
     properties(Dependent)
+        radius(:,1) double {mustBeNonnegative, mustBeNonNan, mustBeNonempty}
         cumulative_fraction(:,1) double {mustBeFinite, mustBeNonempty, mustBeGreaterThanOrEqual(cumulative_fraction,0) mustBeLessThanOrEqual(cumulative_fraction, 1)}
     end
     methods
         function obj=GrainSizeDistribution(grain_size, fraction)
             if nargin>0
-                obj.grainsize=grain_size;
+                obj.diameter=grain_size;
                 if nargin>1
                     if numel(grain_size)==numel(fraction) % must be cumulative
                         obj.cumulative_fraction=fraction;
@@ -25,13 +26,19 @@ classdef GrainSizeDistribution < handle
         function val=get.cumulative_fraction(obj)
             val=cumsum([0;obj.fraction]);
         end
+        function set.radius(obj,val)
+            obj.diameter=val*2;
+        end
+        function val=get.radius(obj)
+            val=obj.diameter.*.5;
+        end
         function val=D(obj,N)
             if any(N(:)>1)
                 N=N/100;
             end
             obj.check_grainsize_fraction();
             [fract,u_id]=unique(obj.cumulative_fraction);
-            val=interp1(fract,obj.grainsize(u_id),N);
+            val=interp1(fract,obj.diameter(u_id),N);
         end
         function val=gsd_int(obj,func,is_radius)
             % integrates a function of grainsize over the GSD
@@ -53,7 +60,9 @@ classdef GrainSizeDistribution < handle
             else
                 mult=1;
             end
-            val=nansum((func(obj.grainsize(1:end-1)*mult)+func(obj.grainsize(2:end)*mult))*.5.*obj.fraction);
+            obj.check_grainsize_fraction();
+            gs=[obj.diameter];
+            val=nansum((func(gs(1:end-1,:)*mult)+func(gs(2:end,:)*mult))*.5.*[obj.fraction],1);
         end
         function val=moment(obj,n,is_radius)
             % Compute n-th statistical moment of GSD 
@@ -73,49 +82,124 @@ classdef GrainSizeDistribution < handle
             val=obj.gsd_int(func,is_radius);
         end
         function val=mean_diam(obj)
+            % Compute the mean diameter
+            % 
+            %   val=mean_diam(obj) computes the mean diameter of the GSD
             val=obj.moment(1);
         end
         function val=mean_radius(obj)
+            % Compute the mean radius
+            %
+            %   val=mean_radius(obj) compute the mean radius from the GSD
             val=obj.moment(1,true);
         end
-        function [ff, x]=form_function(obj, wave_number, radius) % Thorne and Meral, 2008
+        function ff=form_function(obj, wave_number) 
+            % Compute the form function of the sediment
+            %
+            %   ff=form_function(obj, wave_number) computes the form
+            %   function or relative backscattering length (Medwin and
+            %   Clay) given the GSD and the wave_number. wave_number can be
+            %   either the wave number of the acostic signal (1/m) or an
+            %   acoustics.PistonTransducer object. This function implements
+            %   equation 7 from Thorne and Meral (2012). 
             narginchk(2,3);
             if isa(wave_number,'acoustics.PistonTransducer')
                 wave_number=wave_number.wavenumber;
             end
-            if nargin < 3
-                radius=obj.grainsize/2;
-            end
-            x=wave_number.*radius;
-            ff=x.^2.*(1-0.35*exp(-((x-1.5)/0.7).^2)*1+0.5*exp(-((x-1.8)/2.2).^2))./(1+0.9*x.^2);
+            ff=acoustics.GrainSizeDistribution.form_function_static(wave_number.*[obj.radius]);
         end
-        function sigbs=bs_xsection(obj,wave_number) % Medwin and Clay, 1998
-            func=@(x) x.^2.*obj.form_function(wave_number,x).^2;
+        function chi=norm_scatter_xsection(obj,wave_number)
+            % Compute the normalized total scattering cross-section
+            %
+            %   chi=norm_scatter_xsection(obj, wave_number) computes the 
+            %   normalized total scattering cross-section given the GSD and
+            %   the wave_number. wave_number can be either the wave number 
+            %   of the acostic signal (1/m) or an 
+            %   acooustics.PistonTransducer object. This function 
+            %   implements equation 9 from Thorne and Meral (2012). 
+            narginchk(2,3);
+            if isa(wave_number,'acoustics.PistonTransducer')
+                wave_number=wave_number.wavenumber;
+            end
+            chi=acoustics.GrainSizeDistribution.norm_scatter_xsection(wave_number.*[obj.radius]);
+        end
+        function sigbs=bs_xsection(obj,wave_number)
+            % Compute the mean backscattering cross section
+            %
+            %   sigbs=bs_xsection(obj,wave_number) computes the mean
+            %   backscattering cross-section by integrating the
+            %   form-function over the GSD according to equation 4 in Sassi
+            %   et al.(2012). wave_number can be either the wave number 
+            %   of the acostic signal (1/m) or an 
+            %   acooustics.PistonTransducer object.
+            if isa(wave_number,'acoustics.PistonTransducer')
+                wave_number=wave_number.wavenumber;
+            end
+            func=@(x) x.^2.*acoustics.GrainSizeDistribution.form_function_static(wave_number*x).^2;
             sigbs=.25*obj.gsd_int(func,true);
+        end
+        function ks2=ks_squared(obj,wave_number,dens) 
+            %  Compute the ks_squared for volume backscattering computation
+            %
+            %   ks2=ks_squared(obj,wave_number,dens) computes the
+            %   ks_squared which can be used to estimate the volume
+            %   backscatter strength (Sv=10log10(ks2*Ms)). dens is the
+            %   density of the backscattered particles and wave_number can 
+            %   be either the wave number of the acostic signal (1/m) or an 
+            %   acooustics.PistonTransducer object. Computations are done 
+            %   accordint to Sassi et al (2012), eq. 6.
+            if isa(wave_number,'acoustics.PistonTransducer')
+                wave_number=wave_number.wavenumber;
+            end
+            dens=reshape(dens,1,[]);
+            ks2=obj.bs_xsection(wave_number)*3/4/pi./dens./obj.moment(3,true);
+        end
+        function zet=specific_atten(obj,wave_number,dens)
+            % Compute the specific attenuation
+            %
+            %   zet=specific_atten(obj,wave_number,dens) computes the
+            %   specific attenuation by integrating the total scattering 
+            %   cross-section follwing Sassi et al. 2012, eq 9. wave_number 
+            %   can be either the wave number of the acostic signal (1/m) 
+            %   or an acooustics.PistonTransducer object. dens is the
+            %   density of the scattered particles.
+            if isa(wave_number,'acoustics.PistonTransducer')
+                wave_number=wave_number.wavenumber;
+            end
+            dens=reshape(dens,1,[]);
+            func=@(x) x.^2.*acoustics.GrainSizeDistribution.norm_scatter_xsection_static(wave_number*x);
+            zet=3/4./dens.obj.gsd_int(func,true)./obj.moment(3,true);
         end
         function n=n_particles(obj,vol_concentration)
             n=vol_concentration*3/4/pi/obj.moment(3,true);
         end
         function check_grainsize_fraction(obj)
-            assert(numel(obj.grainsize)==numel(obj.fraction)+1,'grainsize should have one more element than fraction')           
-            assert(sum(obj.fraction)-1 < eps,'sum of all fractions should equal 1')
-            assert(issorted(obj.grainsize),'grainsize must be in ascending order')
+            diams=[obj.diameter];
+            fract=[obj.fraction];
+            assert(size(diams,1)==size(fract,1)+1,'grainsize should have one more element than fraction')           
+            assert(all((sum(fract,1)-1).^2 < eps),'sum of fractions in distributions should equal 1')
+            assert(all(issorted(diams)),'diameters must be in ascending order')
         end
         function plot(obj)
-            plot(obj.grainsize,obj.cumulative_fraction*100,'.-')
+            plot([obj.diameter],[obj.cumulative_fraction]*100,'.-')
             set(gca,'xscale','log')
             xlabel('Diameter (m)')
             ylabel('Cumulative fraction (%)')
         end
         function plot_psd(obj)
-            patch(obj.grainsize((0:(numel(obj.grainsize)-2))+[1;2;2;1;1]), obj.fraction'.*([0;0;1;1;0])*100,zeros(5,numel(obj.fraction)));
+            if isscalar(obj)
+                patch(obj.diameter((0:(numel(obj.diameter)-2))+[1;2;2;1;1]), obj.fraction'.*([0;0;1;1;0])*100,zeros(5,numel(obj.fraction)));
+            else
+                gs=[obj.diameter];
+                plot(.5*(gs(1:end-1,:)+gs(2:end,:)),[obj.fraction]*100);
+            end
             set(gca,'xscale','log')
             xlabel('Diameter (m)')
             ylabel('Fraction (%)')
         end
         function plot_form_function(obj,varargin)
             ff=obj.form_function(varargin{:});
-            gs=obj.grainsize();
+            gs=[obj.diameter];
             plot(gs,ff)
             set(gca,'xscale','log','yscale','log')
             xlabel('Diameter (m)')
@@ -126,6 +210,14 @@ classdef GrainSizeDistribution < handle
             obj.plot_psd;
             subplot(2,1,2)
             obj.plot_form_function(varargin{:});
+        end
+    end
+    methods (Static)
+        function ff=form_function_static(x) % Thorne and Meral, 2012, eq.7 
+            ff=x.^2.*(1-0.35*exp(-((x-1.5)/0.7).^2)).*(1+0.5*exp(-((x-1.8)/2.2).^2))./(1+0.9*x.^2);
+        end
+        function chi=norm_scat_xsection_static(x) % Thorne and Meral, 2012, eq. 9
+            chi = 0.29*x.^4./(0.95+1.28*x.^2+0.25*x.^4);        
         end
     end
 end
