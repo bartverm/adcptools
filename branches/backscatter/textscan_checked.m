46b624725da67fe769b1a7b67fd0f9b3875ecf63
bartvermeulen <bartvermeulen@db09c8cb-a954-4599-9796-730ab44c0366>
2019-11-26 11:03:55 +0000

    deleting backscatter branch after merge

--- branches/backscatter/textscan_checked.m
+++
@@ -1,7 +0,0 @@
-function [c position] = textscan_checked(str,varargin)
-	[c position] = textscan(str,varargin{:});
-	if (length(str) ~= position)
-		fprintf(1,'Warning: String not parsed until end\n'); 
-	end
-end
-
