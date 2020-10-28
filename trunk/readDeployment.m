db58b93e7d021b49626fcda7b4000141be3b0134
karlkastner <karlkastner@db09c8cb-a954-4599-9796-730ab44c0366>
2020-10-28 05:04:51 +0000

    readViseaExtern : match visea-file to corresponding ADCP file, fixes bug (Ticket 11) of mismatches when ensemble number is reset to 1 readZDA : use textscan_checked for higher robustness

--- trunk/readDeployment.m
+++ trunk/readDeployment.m
@@ -96,7 +96,7 @@ end
 vfiles=match_and_cat('.*[0-9]{3,3}extern\.dat$'); % search for transect files
 if ~isempty(vfiles)
     disp('Reading VISEA extern files...')
-    outADCP.VISEA_Extern=readViseaExtern(outADCP,vfiles);
+    outADCP.VISEA_Extern=readViseaExtern(outADCP,vfiles,rfiles);
 end
 
 %% Read VISEA log files
