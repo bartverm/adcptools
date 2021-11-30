b1f2c6c9033635164b64abb7e4fd8b92a910efca
bartvermeulen <bartvermeulen@db09c8cb-a954-4599-9796-730ab44c0366>
2021-11-30 15:28:50 +0000

    ADCP.m :Fixed bug with plotting ADCPVerticalPositionFromWaterLevel.m :Added possibility to create a listener to a VMADCP object VMADCP.m :Adds listener to water_level_object to keep objects in sync with water level computation

--- branches/proctrans_update/ADCPVerticalPositionFromWaterLevel.m
+++ branches/proctrans_update/ADCPVerticalPositionFromWaterLevel.m
@@ -3,6 +3,16 @@ classdef ADCPVerticalPositionFromWaterLevel < ADCPVerticalPosition
 %
 %   Subclasses should implement the get_water_level method
 %
+%   Constructor:
+%   obj=ADCPVerticalPositionFromWaterLevel(...) constructs water level
+%   object. Arguments passed to the constructor are handled according to
+%   the type:
+%   - WaterLevel objects are assigned to the water_level property
+%   - double inputs are assigned to the depth_transducer property
+%   - VMADCP objects are used to create a listener that updates the
+%       water_level property, when the water_level_object property is
+%       changed in the VMADCP object.
+%
 %   WaterLevel methods:
 %   get_water_level - returns the water level for each given time
 %   get_depth - compute the depth for a given time and elevation
@@ -20,6 +30,8 @@ classdef ADCPVerticalPositionFromWaterLevel < ADCPVerticalPosition
                     obj.water_level=arg;
                 elseif isa(arg,'double')
                     obj.depth_transducer=arg;
+                elseif isa(arg,'VMADCP')
+                    addlistener(arg,'water_level_object','PostSet',@obj.set_wl)
                 else
                     warning(['Unhandled input of type: ', class(arg)])
                 end
@@ -30,6 +42,9 @@ classdef ADCPVerticalPositionFromWaterLevel < ADCPVerticalPosition
 %                 warning('Set depth of transducer for correct results')
 %             end
             val=obj.water_level.get_water_level(adcp.time)-obj.depth_transducer;
+        end
+        function set_wl(obj, src, varargin)
+            obj.water_level=src.water_level_object;
         end
     end
 end
