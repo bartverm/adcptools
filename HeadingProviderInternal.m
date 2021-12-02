b522bfd8f25f68681beb3e474ca2425f955c6e30
bartvermeulen <bartvermeulen@db09c8cb-a954-4599-9796-730ab44c0366>
2021-12-02 08:39:52 +0000

    Added documentation to HeadingProvider classes and subclasses

--- HeadingProviderInternal.m
+++ HeadingProviderInternal.m
@@ -1,4 +1,11 @@
 classdef HeadingProviderInternal < HeadingProvider
+% returns heading data from ADCP's internal compass
+%
+%   HeadingProviderInternal methods:
+%   heading - returns heading
+%   has_data - returns whether heading data are available
+%
+%   see also: HeadingProvider, HeadingProviderTFiles
     methods(Access=protected)
         function val=get_heading(~,adcp)
             val=double(adcp.raw.heading)/100;
--- HeadingProviderTFiles.m
+++ HeadingProviderTFiles.m
@@ -1,4 +1,11 @@
 classdef HeadingProviderTFiles < HeadingProvider
+% returns heading form t-files
+%
+%   HeadingProviderTFiles methods:
+%   heading - returns heading
+%   has_data - returns whether heading data are available
+%
+%   see also: HeadingProviderInternal, HeadingProvider
     methods(Access=protected)
         function val=get_heading(~,adcp)
             val=adcp.raw.tFiles.corrHead;
