8c6a947d46856238d9930f240e2b96ba8f5d9d4e
Bart Vermeulen <bart.vermeulen@wur.nl>
2022-11-17 17:12:38 +0100

    Implementing basic ADCP functionality for Sontek data

--- +sontek/ADCP.m
+++ +sontek/ADCP.m
@@ -1,10 +1,15 @@
 classdef ADCP < ADCP
     properties
-        raw
+        % Sontek raw data
+        raw(1,1) struct = struct
     end
     methods
         function obj = ADCP(varargin)
             obj = obj@ADCP(varargin{:});
+            obj.tilts_provider = sontek.TiltsInternal;
+            obj.heading_provider = sontek.HeadingInternal;
+            obj.instrument_matrix_provider = ...
+                sontek.InstrumentMatrixInternal;
             if nargin>0
                 searchstr=varargin{1};
                 if ischar(searchstr) ||...
@@ -21,25 +26,31 @@ classdef ADCP < ADCP
                 else
                     error('Input should be char or cell cell of char')
                 end
-                for cf=1:numel(files)
-                    if exist(files{cf},'file')
+                count_good_reads = 0;
+                n_files = numel(files);
+                tmp_struct = cell(n_files, 1);
+                for count_file = 1:n_files
+                    if exist(files{count_file}, 'file')
                         try
-                            tmp=load(files{cf});
+                            tmp=load(files{count_file});
                         catch err
                             warning(['Could not read file ', ...
-                                files{cf}, ': ',err.message])
+                                files{count_file}, ': ',err.message])
                             continue
                         end
                     else
-                        warning(['Could not find file: ', files{cf}])
+                        warning(['Could not find file: ',...
+                            files{count_file}])
                         continue
                     end
-                    obj(end).raw=tmp;
-                    obj(end).raw.filename=files{cf};
-                    obj(end+1)=sontek.ADCP; %#ok<AGROW>
+                    count_good_reads = count_good_reads + 1;
+                    tmp_struct{count_good_reads} = tmp;
+                    tmp_struct{count_good_reads}.filename = ...
+                        files{count_file};                   
                 end
-                obj(end)=[];
-                if isempty(obj)
+                tmp_struct(count_good_reads + 1 : end)=[];
+                obj.raw = obj.cat_structs([tmp_struct{:}]);
+                if count_good_reads == 0
                     warning('Could not read any files')
                 end
             end
@@ -56,29 +67,80 @@ classdef ADCP < ADCP
         end
         function get_salinity(obj)
         end
-        function get_temperature(obj)
+        function out = get_temperature(obj)
+            out = obj.raw.System.Temperature';
         end
-        function get_distmidfirstcell(obj)
+        function out = get_distmidfirstcell(obj)
+            out = obj.raw.System.Cell_Start';
         end
-        function get_time(obj)
+        function out = get_time(obj)
+            out = datetime(datevec(datenum(2000,1,1)+obj.raw.System.Time/24/3600))';
         end
-        function get_cellsize(obj)
+        function out = get_cellsize(obj)
+            out = obj.raw.System.Cell_Size';
         end
-        function get_beam_angle(obj)
+        function val = get_beam_angle(obj)
+            bom = obj.instrument_matrix_provider.beam_orientation_matrix(obj);
+            val = acosd(bom(:,:,:,3));
         end
-        function get_coordinate_system(obj)
+        function val = get_coordinate_system(obj)
+            cor=obj.raw.Setup.coordinateSystem;
+            val = repmat(CoordinateSystem.Beam,size(cor));
+            val(cor==1) = CoordinateSystem.Ship;
+            val(cor==2) = CoordinateSystem.Earth;
         end
-        function get_ncells(obj)
+        function out = get_ncells(obj)
+            out = size(obj.raw.WaterTrack.Velocity,1);
         end
-        function get_nensembles(obj)
+        function out = get_nensembles(obj)
+            out = size(obj.raw.System.Time,1);
         end
-        function get_nbeams(obj)
+        function out = get_nbeams(obj)
+            out = size(obj.raw.WaterTrack.Velocity,2);
         end
     end
     methods
-        function velocity(obj)
+        function vel = velocity(obj,dst,filter)
         end
-        function xform(obj)
+        function val = xform(obj,dst,src,varargin)
+        end
+    end
+    methods(Static, Access = private)
+        function out = cat_structs(in)
+            % concatenates field in different sontek structures
+            fields = fieldnames(in);
+            for cf = fields'
+                current_field = cf{1};
+                if isstruct(in(1).(current_field))
+                    out.(current_field) = ...
+                        sontek.ADCP.cat_structs([in.(current_field)]);
+                else
+                    if isscalar(in(1).(current_field))
+                        out.(current_field) = [in.(current_field)];
+                    elseif ischar(in(1).(current_field))
+                        out.(current_field) = {in.(current_field)};
+                    elseif ismatrix(in(1).(current_field))
+                        out.(current_field) = vertcat(in.(current_field));
+                    else
+                        out.(current_field) = cat(3,in.(current_field));
+                    end
+                end
+            end
+            % create a file id to map per-file scalar values to ensembles
+            if isfield(in,'System') && all(cellfun(@(x) isfield(x,'Time'),{in.System}))
+                n_ens = cellfun(@(x) size(x.Time,1), {in.System});
+                out.file_id = zeros(size(out.System.Time,1),1);
+                out.file_id(1) = 1;
+                out.file_id(cumsum(n_ens(1:end-1))+1) = 1;
+                out.file_id = cumsum(out.file_id);
+            end
+            % fix concatenation of matrices
+            if isfield(in,'Transformation_Matrices')
+                out.Transformation_Matrices.Frequency = ...
+                    reshape(out.Transformation_Matrices.Frequency, 3,[]);
+                out.Transformation_Matrices.Matrix = ...
+                    reshape(out.Transformation_Matrices.Matrix,4,4,3,[]);
+            end
         end
     end
 end
