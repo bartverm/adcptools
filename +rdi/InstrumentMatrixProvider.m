c39407981154953799fdcb86ac0f68e8a86888f5
Bart Vermeulen <bart.vermeulen@wur.nl>
2022-11-23 13:08:39 +0100

    Adding some documentation and explanation about conventions in ADCP geometry and positioning methods

--- +rdi/InstrumentMatrixProvider.m
+++ +rdi/InstrumentMatrixProvider.m
@@ -4,11 +4,14 @@ classdef InstrumentMatrixProvider < InstrumentMatrixProvider
             val = obj.i2b_matrix(adcp);
             val(:,:,:,4) = [];
             val = -val;
+            % Note that the instrument matrix is defined for the ADCP in a
+            % downlooking position, which means that the positive z
+            % component results in a vector pointing toward the adcp.
+            % Negating it, we have a vector point away from the ADCP.
         end 
         function val = get_vertical_range_to_cell(~,adcp)
-            % turns out rdi defines the instrument matrix for a downward
-            % looking ADCP, so the vertial range is increasing downwards,
-            % therefor negative
+            % rdi defines the instrument matrix for a downward looking ADCP
+            % so the vertial range is increasing downwards
             val = -(adcp.distmidfirstcell+reshape(0:max(adcp.ncells)-1,[],1).*adcp.cellsize);
         end
     end
