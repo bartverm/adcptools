b3ec0e13d57dc395f5b0c720a21a359628354297
Bart Vermeulen <bart.vermeulen@wur.nl>
2023-03-16 12:05:20 +0100

    Removing nmea toolbox to include it later as submodule

--- +nmea/LatLonField.m
+++
@@ -1,8 +0,0 @@
-classdef LatLonField < nmea.Field
-    methods
-        function obj = LatLonField()
-            obj.post_process =...
-                @(x) ((x{3}=='N' | x{3}=='E')*2-1).*(x{1}+x{2}/60);
-        end
-    end
-end
