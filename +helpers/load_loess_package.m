97c5ae5278ac60b985cc42d491d3e58e68f1469d
Bart Vermeulen <bart.vermeulen@wur.nl>
2023-03-31 17:10:18 +0200

    Improving testing of loess package loading

--- +helpers/load_loess_package.m
+++ +helpers/load_loess_package.m
@@ -4,15 +4,25 @@ function tf = load_loess_package()
 %   tf = helpers.load_loess_package() loads the nmea package if it was not 
 %   loaded yet. Returns whether the nmea package is available after the 
 %   call to the function.
-    tf = loess_is_loaded();
+
+    % Case loess exists and runs
+    tf = loess_exists && loess_runs;
     if tf
         return
     end
+
     adcptools_path = fileparts(which('ADCP'));
     loess_path = fullfile(adcptools_path, "loess_submodule");
-    addpath(loess_path)
-    tf = loess_is_loaded();
-    if ~tf && exist("compile_loess.m",'file') == 2
+
+
+    % If loess does not exist, add path
+    if ~loess_exists
+        addpath(loess_path);
+    end
+
+    % if loess does not exist and run, and the compile script exist,
+    % compile
+    if ~(loess_exists && loess_runs) && exist("compile_loess.m",'file') == 2
         current_path = pwd;
         cd(loess_path)
         try
@@ -21,11 +31,21 @@ function tf = load_loess_package()
             warning('Tried to compile loess but failed.')
         end
         cd(current_path);
-        tf = loess_is_loaded();
     end
 
+    % check if loess exists and if it runs
+    tf = loess_exists && loess_runs;
 end
 
-function tf = loess_is_loaded()
+
+function tf = loess_exists()
     tf = exist(['loess.', mexext],'file') == 3;
+end
+
+function tf = loess_runs()
+    try
+        loess
+    catch err
+        tf = strcmp(err.identifier,'loess:nargin');
+    end
 end
---
+++ +tests/test_load_loess_package.m
@@ -0,0 +1,21 @@
+classdef test_load_loess_package < matlab.unittest.TestCase
+    methods(Test)
+        function test_run_precompiled(testCase)
+            % tests adding loess package to path
+            testCase.assertTrue(helpers.load_loess_package);
+        end
+        function test_compile(testCase)
+            % tests compiling loess
+            testCase.applyFixture(tests.RemoveFileFixture(...
+                fullfile('loess_submodule',['loess.',mexext])));
+            testCase.assertTrue(helpers.load_loess_package);
+        end
+        function test_preload(testCase)
+            % tests adding nmea package to path while package is already
+            % loaded
+            helpers.load_loess_package;
+            testCase.assertTrue(helpers.load_loess_package);
+        end
+        
+    end
+end
