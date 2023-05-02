f8a2ad0d9728449c23ea1cc35e6fd86998ea2ad9
Bart Vermeulen <bart.vermeulen@wur.nl>
2023-05-02 12:32:49 +0200

    Adding test that generates documentation

--- +tests/run_tests.m
+++ +tests/run_tests.m
@@ -1,21 +1,20 @@
-  import matlab.unittest.TestRunner;
-  import matlab.unittest.plugins.CodeCoveragePlugin;
-  import matlab.unittest.plugins.TestReportPlugin;
-  import matlab.unittest.plugins.XMLPlugin;
-  import matlab.unittest.plugins.codecoverage.CoverageReport;
- 
-  suite = testsuite(pwd, 'IncludeSubfolders', true);
-  
-  [~,~] = mkdir('code-coverage');
-  [~,~] = mkdir('test-results');
-  
-  runner = TestRunner.withTextOutput();
-  runner.addPlugin(TestReportPlugin.producingPDF('test-results/results.pdf'));
-  runner.addPlugin(CodeCoveragePlugin.forFolder({'.'}, 'IncludingSubfolders', true,...
-      "Producing",CoverageReport('code-coverage')));
-  
-  results = runner.run(suite);
-  display(results);
-  
-  assertSuccess(results);
-  
+import matlab.unittest.TestRunner;
+import matlab.unittest.plugins.CodeCoveragePlugin;
+import matlab.unittest.plugins.TestReportPlugin;
+import matlab.unittest.plugins.XMLPlugin;
+import matlab.unittest.plugins.codecoverage.CoverageReport;
+
+suite = testsuite('+tests', 'IncludeSubfolders', false);
+
+[~,~] = mkdir('code-coverage');
+[~,~] = mkdir('test-results');
+
+runner = TestRunner.withTextOutput();
+runner.addPlugin(TestReportPlugin.producingPDF('test-results/results.pdf'));
+runner.addPlugin(CodeCoveragePlugin.forFolder({'.'}, 'IncludingSubfolders', true,...
+    "Producing",CoverageReport('code-coverage')));
+
+results = runner.run(suite);
+display(results);
+
+assertSuccess(results);
