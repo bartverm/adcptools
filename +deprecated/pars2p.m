205da9f395cd877648b9613db4fb2060d6b4730f
Henk Jongbloed <henkjongbloed@live.com>
2023-05-08 18:03:21 +0200

    Moved some functions to deprecated

---
+++ +deprecated/pars2p.m
@@ -0,0 +1,5 @@
+function p = pars2p(pars)
+
+p = reshape(pars', 1, [])';
+
+end
--- pars2p.m
+++
@@ -1,6 +0,0 @@
-function p = pars2p(pars)
-
-pars = pars';
-p = pars(:);%reshape(squeeze(p(:,1,1)) ,[size(Mb0,2), obj.mesh.ncells])';
-
-end
