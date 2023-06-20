3d998285987c1cf4681797ff971611052e72ea2c
Bart Vermeulen <bart.vermeulen@wur.nl>
2023-06-20 14:26:37 +0200

    Fixing compilation problems

--- +tests/test_load_loess_package.m
+++ +tests/test_load_loess_package.m
@@ -8,6 +8,7 @@ classdef test_load_loess_package < matlab.unittest.TestCase
             % tests adding nmea package to path while package is already
             % loaded
             helpers.load_loess_package;
+            loess
             testCase.assertTrue(helpers.load_loess_package);
         end
     end
